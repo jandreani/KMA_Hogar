c4ebe03e2b5db2c5e5fe5016b632c0cc51cb5ea5
jandreani <j.andreanib@gmail.com>
2012-10-29 04:09:35 -0300

    1 semana, Version 1.1

---
+++ WebBodegas/Formularios/frmMenuLogin.aspx
@@ -0,0 +1,100 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmMenuLogin.aspx.cs" Inherits="webBodegas.Formularios.frmMenuLogin" %>
+
+<%@ Register assembly="AjaxControlToolkit" namespace="AjaxControlToolkit" tagprefix="asp" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .style1
+        {
+            width: 100%;
+        }
+        .style2
+        {
+            width: 140px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:ToolkitScriptManager ID="ToolkitScriptManager1" runat="server">
+        </asp:ToolkitScriptManager>
+        <br />
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                <table class="style1">
+                    <tr>
+                        <td>
+                            <table class="style1">
+                                <tr>
+                                    <td class="style2">
+                                        <asp:Label ID="Label1" runat="server" Text="USUARIO"></asp:Label>
+                                    </td>
+                                    <td>
+                                        <asp:TextBox ID="txtusuario" runat="server"></asp:TextBox>
+                                        <asp:CompareValidator ID="valusuario" runat="server" 
+                                            ControlToValidate="txtusuario" Display="None" ErrorMessage="Ingrese el usuario"></asp:CompareValidator>
+                                        <asp:ValidatorCalloutExtender ID="valusuario_ValidatorCalloutExtender" 
+                                            runat="server" Enabled="True" TargetControlID="valusuario">
+                                        </asp:ValidatorCalloutExtender>
+                                    </td>
+                                    <td>
+                                        &nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td class="style2">
+                                        <asp:Label ID="Label2" runat="server" Text="CONTRASEÑA"></asp:Label>
+                                    </td>
+                                    <td>
+                                        <asp:TextBox ID="txtpassword" runat="server" TextMode="Password"></asp:TextBox>
+                                        <asp:CompareValidator ID="valpassword" runat="server" 
+                                            ControlToValidate="txtpassword" Display="None" 
+                                            ErrorMessage="Ingrese el password"></asp:CompareValidator>
+                                        <asp:ValidatorCalloutExtender ID="valpassword_ValidatorCalloutExtender" 
+                                            runat="server" Enabled="True" TargetControlID="valpassword">
+                                        </asp:ValidatorCalloutExtender>
+                                    </td>
+                                    <td>
+                                        &nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td class="style2">
+                                        &nbsp;</td>
+                                    <td>
+                                        <asp:Button ID="btnaceptar" runat="server" Text="Aceptar" />
+                                    </td>
+                                    <td>
+                                        &nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td class="style2">
+                                        &nbsp;</td>
+                                    <td>
+                                        &nbsp;</td>
+                                    <td>
+                                        &nbsp;</td>
+                                </tr>
+                                <tr>
+                                    <td class="style2">
+                                        &nbsp;</td>
+                                    <td>
+                                        &nbsp;</td>
+                                    <td>
+                                        &nbsp;</td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebBodegas/Formularios/frmMenuLogin.aspx.designer.cs
@@ -0,0 +1,123 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace webBodegas.Formularios {
+    
+    
+    public partial class frmMenuLogin {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control ToolkitScriptManager1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ToolkitScriptManager ToolkitScriptManager1;
+        
+        /// <summary>
+        /// Control UpdatePanel1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Control txtusuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtusuario;
+        
+        /// <summary>
+        /// Control valusuario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator valusuario;
+        
+        /// <summary>
+        /// Control valusuario_ValidatorCalloutExtender.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ValidatorCalloutExtender valusuario_ValidatorCalloutExtender;
+        
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Control txtpassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtpassword;
+        
+        /// <summary>
+        /// Control valpassword.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator valpassword;
+        
+        /// <summary>
+        /// Control valpassword_ValidatorCalloutExtender.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::AjaxControlToolkit.ValidatorCalloutExtender valpassword_ValidatorCalloutExtender;
+        
+        /// <summary>
+        /// Control btnaceptar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnaceptar;
+    }
+}
